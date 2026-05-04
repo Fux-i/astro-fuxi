b5ed7218531dbce5e552ccd5b3f55e6689b8f0e6
fuxi <1538130391@qq.com>
2026-05-04 20:13:53 +0800

    content config

---
+++ src/config.ts
@@ -0,0 +1,26 @@
+import IconMail from "@/assets/icons/IconMail.svg";
+import IconZhihu from "@/assets/icons/IconZhihu.svg"
+import IconGitHub from "@/assets/icons/IconGitHub.svg";
+
+export const SITE = {
+  title_suffix: " | Fuxi's blog",
+  socials: [
+    {
+      name: "Mail",
+      href: "mailto:1538130391@qq.com",
+      linkTitle: `Send an email to Fuxi`,
+      icon: IconMail,
+    }, {
+      name: "Zhihu",
+      href: "https://www.zhihu.com/people/ju-jiu-31-72/posts",
+      linkTitle: `Fuxi on Zhihu`,
+      icon: IconZhihu,
+    },
+    {
+      name: "GitHub",
+      href: "https://github.com/fux-i",
+      linkTitle: `Fuxi on GitHub`,
+      icon: IconGitHub,
+    },
+  ]
+}
